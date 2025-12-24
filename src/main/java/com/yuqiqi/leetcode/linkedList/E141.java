0f9b8b0317e7e77c9d3de1e9f4a465b169ad2fd5
yuqiqi <2328441709@qq.com>
2025-12-24 22:10:00 +0800

    力扣141 142 弗洛伊德判圈算法  弗洛伊德龟兔赛跑算法：快慢指针，相遇的话一定成环，成环后慢指针回起点，然后一起同速度移动，再次相遇的地方就是环的入口。（一个小小的公式推导）

---
+++ src/main/java/com/yuqiqi/leetcode/linkedList/E141.java
@@ -0,0 +1,31 @@
+package com.yuqiqi.leetcode.linkedList;
+
+import com.yuqiqi.bean.ListNode;
+
+/**
+ * ⭐链表成环
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ */
+public class E141 {
+    /**
+     * 弗洛伊德龟兔赛跑算法   快慢指针相遇则成环，  成环后龟返回，兔和龟匀速，再次相遇即为环的入口
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null){
+            return false;
+        }
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while(p2 != null && p2.next != null){  //兔子没走到终点
+            p1 = p1.next;
+            p2 = p2.next.next; //先走  然后再判断
+            if (p1 == p2){
+                return true;
+            }
+        }
+        return false;
+    }
+}
