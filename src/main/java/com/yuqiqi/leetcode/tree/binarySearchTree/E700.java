5e117ab0970474312b49a25afa59fe1c7198afe5
yuqiqi <2328441709@qq.com>
2026-01-07 22:56:41 +0800

    力扣700  查找二叉搜索树中的元素

---
+++ src/main/java/com/yuqiqi/leetcode/tree/binarySearchTree/E700.java
@@ -0,0 +1,45 @@
+package com.yuqiqi.leetcode.tree.binarySearchTree;
+
+import com.yuqiqi.dataStructure.tree.TreeNode;
+
+/**
+ * 二叉搜素树查找   给定二叉搜索树（BST）的根节点 root 和一个整数值 val。
+ * 你需要在 BST 中找到节点值等于 val 的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 null 。
+ */
+public class E700 {
+    /**
+     * 递归法
+     * @param root
+     * @param val
+     * @return
+     */
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null){
+            return null;
+        }
+        if (root.val == val){
+            return root;
+        }
+        if (val < root.val){
+            return searchBST(root.left,val);
+        }
+        return searchBST(root.right, val);
+    }
+
+    /**
+     * 迭代
+     */
+    public TreeNode searchBST2(TreeNode root, int val){
+        TreeNode p = root;
+        while(p != null){
+            if (val < p.val){
+                p = p.left;
+            }else if (val > p.val){
+                p = p.right;
+            }else {
+                return p;
+            }
+        }
+        return null;
+    }
+}
