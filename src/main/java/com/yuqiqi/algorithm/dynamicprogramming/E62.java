937cbbd168985d5a311e031e9ba38a77a8908ed1
yuqiqi <2328441709@qq.com>
2026-01-30 11:45:29 +0800

    动态规划  01背包问题   行是物品种类  列是背包容量   dp代表当前容量下的最高价值选择

--- src/main/java/com/yuqiqi/algorithm/dynamicprogramming/E62.java
+++ src/main/java/com/yuqiqi/algorithm/dynamicprogramming/E62.java
@@ -12,7 +12,7 @@ public class E62 {
     //动态规划   ⭐要走到第i行第j列总的走法数 = i-1,j + i,j - 1   全横是1  全列也是1
     //思路：先缩小范围来看  先看1*2的 2*1的  再看2 * 2 的 就可以发现规律  然后动态规划即可
     public static int uniquePaths(int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
+        int[][] dp = new int[m + 1][n + 1];   //其实这里m行n列就行
         //初始化行和列
         for (int i = 0; i <= n; i++) {
             dp[1][i] = 1;
@@ -29,7 +29,24 @@ public class E62 {
         return dp[m][n];
     }
 
+    //降维
+    public static int uniquePaths2(int m, int n){
+        int[] dp = new int[n]; //n列的1维数组
+        //初始化
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] = dp[j] + dp[j - 1];
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[n - 1];
+    }
+
     public static void main(String[] args) {
-        System.out.println(uniquePaths(2, 2));
+        System.out.println(uniquePaths2(3, 7));
+
     }
 }
---
+++ src/main/java/com/yuqiqi/algorithm/dynamicprogramming/Knapsack.java
@@ -0,0 +1,77 @@
+package com.yuqiqi.algorithm.dynamicprogramming;
+
+import java.util.Arrays;
+
+/**
+ * ⭐01背包问题  物品不能分割   不能用贪心算法  得用动态规划
+ *  * n个物品都是固体，有重量和价值
+ *  * 要取走不超过10克的物品
+ *  * 每次可以不拿或者全拿
+ */
+public class Knapsack {
+    static class Item {
+        int index;
+        int weight;  //物品重量
+        int value;  //物品价格
+
+        public Item(int index, int weight, int value) {
+            this.index = index;
+            this.weight = weight;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Item{" +
+                    "index=" + index +
+                    ", weight=" + weight +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        Item[] items = new Item[]{
+                new Item(0, 4, 24),
+                new Item(1, 8, 160),
+                new Item(2, 2, 4000),
+                new Item(3, 6, 108),
+                new Item(4, 1, 4000),
+        };
+        System.out.println(select(items, 10)); //假设最大能装10kg
+    }
+
+    public static int select(Item[] items , int total){
+        //⭐可降维  手动遍历物品数组  背包重量从右向左遍历
+         int[][] dp = new int[items.length][total + 1];  //行是物品种类   列是背包容量   dp代表当前容量下的最大利益
+        /**
+         * 递推关系
+         * if 装不下 {
+         *           dp[i][j] = dp[i-1][j]
+         * }else{
+         *           dp[i][j] = max(dp[i-1][j],item.value + dp[i - 1][j - item.weight])  本身的价值和新放入的价值求和取一个最大值
+         *       }
+         */
+        //处理第一行数据   填满
+        Item item0 = items[0];
+        for (int j = 0; j < total + 1; j++) {
+            if (j >= item0.weight){  //放得下
+                dp[0][j] = item0.value;
+            }else {  //放不下
+                dp[0][j] = 0;
+            }
+        }
+//        System.out.println(Arrays.deepToString(dp));
+        //处理剩下的行（物品）
+        for (int i = 1; i < dp.length; i++) {  //从第二行开始
+            for (int j = 0; j < total + 1; j++) {
+                if (j >= items[i].weight){  //装得下第j个物品
+                    dp[i][j] = Math.max(dp[i - 1][j],items[i].value + dp[i - 1][j - items[i].weight]);
+                }else {  //装得下第j个物品  继承上一行
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[items.length - 1][total];  //返回最右下角的值
+    }
+}
